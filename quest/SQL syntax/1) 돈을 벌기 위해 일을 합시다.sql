aee519993330b84c4307b615f3ddc482d2909d9c
HellSkk <hellskk.dev@gmail.com>
2025-05-07 14:19:34 +0900

    퀘스트 1

---
+++ quest/SQL syntax/1) 돈을 벌기 위해 일을 합시다.sql
@@ -0,0 +1,15 @@
+-- 1.
+select name,
+    position
+from sparta_employees
+-- 2.
+select distinct position
+from sparta_employees
+-- 3.
+select name
+from sparta_employees
+where salary between 40000 and 60000
+-- 4.
+select *
+from sparta_employees
+where hire_date < '2023-01-01'
